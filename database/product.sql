47676b3ad6e15097e3223844b0ddd2d77b48d2a3
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-10-14 21:26:36 -0300

    chore(product.sql): add product table

---
+++ database/product.sql
@@ -0,0 +1,12 @@
+CREATE TABLE product
+(
+  id UUID PRIMARY KEY NOT NULL,
+  price_in_cents INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL
+)
+
+CREATE UNIQUE INDEX product_id_idx
+  ON product
+  USING btree
+  (id);
