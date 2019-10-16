dbba342065b653202e5a5ab5e33d98a1ff010176
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-10-15 22:23:08 -0300

    chore(database/fill-person): add fill-person sql script

---
+++ database/fill-person.sql
@@ -0,0 +1,6 @@
+INSERT INTO person (id, first_name, last_name, date_of_birth) values ('ae157295-a242-4c50-9f58-c0843ee6ebf2', 'Billy', 'The Kid', '1859-11-23');
+INSERT INTO person (id, first_name, last_name, date_of_birth) values ('9a98fab9-d22a-49d6-816d-d081ca59018a', 'Jesse', 'James', '1847-09-5');
+INSERT INTO person (id, first_name, last_name, date_of_birth) values ('aa19acd6-19f7-4213-93bc-9daf8eed7570', 'Butch', 'Cassidy', '1866-04-13');
+INSERT INTO person (id, first_name, last_name, date_of_birth) values ('41c6ebf9-77d6-46a2-b087-f8413edaca26', 'Sundance', 'Kid', '1867-11-07');
+INSERT INTO person (id, first_name, last_name, date_of_birth) values ('9f5d0b00-d910-4e14-bfb1-0c53a99c1c94', 'Regular Guy', 'That borned at Black Friday day', '2019-11-25');
+
