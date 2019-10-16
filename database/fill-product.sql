ffb8fd012ce99868bdac231990648e2dfbddf496
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-10-15 22:42:20 -0300

    chore(database/fill-product): add fill-product sql script

---
+++ database/fill-product.sql
@@ -0,0 +1,7 @@
+INSERT INTO product (id, price_in_cents, title, description) VALUES ('e31d5fbe-4413-40e8-b42c-97532b0a340c', 666, 'Colt Paterson', 'Used by Texas Rangers');
+INSERT INTO product (id, price_in_cents, title, description) VALUES ('d01f461d-405f-457d-b774-dc54bbe70b05', 1204, 'Henry 1860', 'Used at Battle of the Little Bighorn');
+INSERT INTO product (id, price_in_cents, title, description) VALUES ('78ed6bbd-fc8d-418d-8caf-e5bc19e1b38b', 780, 'Colt Single Action Army', 'The deadliest weapon on the American Frontier');
+INSERT INTO product (id, price_in_cents, title, description) VALUES ('4e9cfaea-8f20-4613-85ee-42911e1ada92', 850, 'The 1911A1', 'M1911A1 is the most reliable pistol in harsh conditions that is possible to make');
+INSERT INTO product (id, price_in_cents, title, description) VALUES ('32bc4c89-2a26-4c2a-b098-cba0c8e58c5f', 456, 'Colt 1851 Navy', 'The preferred revolver of James Wild Bill Hickok');
+INSERT INTO product (id, price_in_cents, title, description) VALUES ('9c1ad5c4-b7e8-4686-a7bc-480640496f10', 2510, '1873 Springfield Trapdoor', 'Many blame this flaw as one of the reasons the 7th Calvary was routed at the Little Bighorn');
+INSERT INTO product (id, price_in_cents, title, description) VALUES ('7ced6e1e-64ca-46b6-ba61-b394b252e652', 6514, 'Winchester Model 1873', 'The most famous and recognizable rifle of the Old West');
