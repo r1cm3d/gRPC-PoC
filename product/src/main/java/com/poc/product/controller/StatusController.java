eba539cd77cc40e155aa7193962ccdfc4415f259
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-10-21 22:02:59 -0300

    feat(StatusController): add status endpoint

---
+++ product/src/main/java/com/poc/product/controller/StatusController.java
@@ -0,0 +1,15 @@
+package com.poc.product.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/status")
+public class StatusController {
+
+	@GetMapping
+	public String status() {
+		return "OK";
+	}
+}
