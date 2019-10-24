44bb5bc35698adf2e131a5cd97bc1ed7128bec6d
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-10-23 21:07:13 -0300

    refactor(Application): rename ApplicationProduct to Application

--- product/src/main/java/com/poc/product/Application.java
+++ product/src/main/java/com/poc/product/Application.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Generated
-public class ProductApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
+		SpringApplication.run( Application.class, args);
 	}
 
 }
