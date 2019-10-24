11d0b89da3d08f5b47e5fce9912497f41c2ed9d5
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-10-23 21:05:45 -0300

    chore(ProductApplication): add lombok @Generated annotation to avoid coverage error because this class was automatically generated

--- product/src/main/java/com/poc/product/ProductApplication.java
+++ product/src/main/java/com/poc/product/ProductApplication.java
@@ -1,9 +1,12 @@
 package com.poc.product;
 
+import lombok.Generated;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Generated
 public class ProductApplication {
 
 	public static void main(String[] args) {
