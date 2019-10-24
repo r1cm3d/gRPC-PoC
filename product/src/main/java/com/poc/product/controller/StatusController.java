a6cc71684282c5615db8dffd5b4e4310861c9e6e
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-10-23 21:02:24 -0300

    feat(StatusController): add lombok @NoArgsConstructor with private access at StatusController avoiding coverage error at constructor

--- product/src/main/java/com/poc/product/controller/StatusController.java
+++ product/src/main/java/com/poc/product/controller/StatusController.java
@@ -1,11 +1,16 @@
 package com.poc.product.controller;
 
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.NoArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/status")
+@NoArgsConstructor(access = PRIVATE)
 public class StatusController {
 
 	@GetMapping
